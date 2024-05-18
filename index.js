cd21d2b388fb1fa468151b4c47bbfe84175752d3
hrishikesh <hrishikeshajith0@gmail.com>
2024-05-19 01:42:21 +0530

    added error handling middleware

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const errorhandler = require("./middleware/errorHandler")
 const dotenv = require("dotenv").config()
 const app = express()
 
@@ -6,6 +7,7 @@ const port = process.env.PORT || 5000
 
 app.use(express.json())
 app.use("/api/inventory", require("./routes/inventoryRoutes"))
+app.use(errorhandler)
 
 app.listen(port, () => {
     console.log(`Server is running at: ${port}`)
