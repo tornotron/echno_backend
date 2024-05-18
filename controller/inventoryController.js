076cf416563b8226da01ac418cad0399a99bc7a8
hrishikesh <hrishikeshajith0@gmail.com>
2024-05-19 01:43:25 +0530

    added express async handler to simplify try catch blocks

--- controller/inventoryController.js
+++ controller/inventoryController.js
@@ -1,37 +1,43 @@
+const asyncHandler = require("express-async-handler")
 //@desc Get all the inventory
 //@route GET /api/inventory
 //@access public
-const getInventory = (req, res) => {
+const getInventory = asyncHandler(async(req, res) => {
     res.status(200).json({ message: "Get all inventory" })
-}
+})
 
 //@desc Create new inventory
 //@route POST /api/inventory
 //@access public
-const createInventory = (req, res) => {
+const createInventory = asyncHandler(async (req, res) => {
     console.log("The request body:", req.body)
-    res.status(201).json({ message: "Get all inventory" })
-}
+    const {item, location} = req.body
+    if(!item || !location) {
+        res.status(400)
+        throw new Error("All fields are mandatory")
+    }
+    res.status(201).json({ message: `Item added to ${location} inventory: ${item}` })
+})
 
 //@desc Get an inventory
 //@route GET /api/inventory/:id
 //@access public
-const getaInventory = (req, res) => {
+const getaInventory = asyncHandler(async(req, res) => {
     res.status(200).json({ message: `Get inventory for ${req.params.id}` })
-}
+})
 
 //@desc update an inventory
 //@route PUT /api/inventory/:id
 //@access public
-const updateInventory = (req, res) => {
+const updateInventory = asyncHandler(async(req, res) => {
     res.status(200).json({ message: `updated ${req.params.id}` })
-}
+})
 
 //@desc delete an inventory
 //@route PUT /api/inventory/:id
 //@access public
-const deleteInventory = (req, res) => {
+const deleteInventory = asyncHandler(async(req, res) => {
     res.status(200).json({ message: `Delete inventory for ${req.params.id}` })
-}
+})
 
 module.exports = { getInventory, createInventory ,getaInventory, updateInventory, deleteInventory}
