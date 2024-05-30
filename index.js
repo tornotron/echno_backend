8507e51b7f84e037e4b0009222528c79c0379734
hrishikesh <hrishikeshajith0@gmail.com>
2024-05-30 21:30:52 +0530

    added cluster

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 import 'dotenv/config'
+import cluster from 'node:cluster';
+import { availableParallelism } from 'node:os';
 import express, { json } from "express";
 import { errorHandler } from "./middleware/errorHandler.js";
 import inventoryRoutes from './routes/inventoryRoutes.js';
-import {fetchUserIds} from './db/database.js';
+import { fetchUserIds } from './db/database.js';
+
+
+const numCPUs = availableParallelism();
 
 const app = express()
 const port = process.env.PORT
@@ -10,8 +15,14 @@ app.use(json())
 app.use("/api/inventory", inventoryRoutes)
 app.use(errorHandler)
 
-app.listen(port, () => {
-    console.log(`Server is running at port: ${port}`)
-})
-// let userIds = await fetchUserIds()
-// console.log(userIds)
+
+
+if (cluster.isPrimary) {
+    for (let i = 0; i < numCPUs; i++) {
+        cluster.fork()
+    }
+} else {
+    app.listen(port, () => {
+        console.log(`Server is running at port: ${port} with pid: ${process.pid}`)
+    })
+}
